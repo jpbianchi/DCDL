9fd1a5dd92c9ff5cee1a54ae66b6023515d4d951
Jean-Pierre Bianchi <jpbianchi@gmail.com>
2023-10-08 20:34:59 +0000

    Week 3 conf learning done

--- course/week3/conflearn_project/flow_baseline.py
+++ course/week3/conflearn_project/flow_baseline.py
@@ -133,3 +133,55 @@ if __name__ == "__main__":
   You can specify a run id as well.
   """
   flow = TrainBaseline()
+# first, download the big lfs files
+# cd data
+# sudo git lfs pull (you will have to provideyour github id and pw)
+
+
+# python flow_baseline.py --no-pylint run
+# Metaflow 2.6.0 executing TrainBaseline for user:gitpod
+# Validating your flow...
+#     The graph looks good!
+# 2023-10-06 22:28:26.930 Workflow starting (run-id 1696631306926666):
+# 2023-10-06 22:28:26.948 [1696631306926666/start/1 (pid 20805)] Task is starting.
+# 2023-10-06 22:28:29.874 [1696631306926666/start/1 (pid 20805)] Task finished successfully.
+# 2023-10-06 22:28:29.888 [1696631306926666/init_system/2 (pid 20851)] Task is starting.
+# 2023-10-06 22:28:32.447 [1696631306926666/init_system/2 (pid 20851)] Split is train
+# 2023-10-06 22:28:32.541 [1696631306926666/init_system/2 (pid 20851)] Split is dev
+# 2023-10-06 22:28:32.600 [1696631306926666/init_system/2 (pid 20851)] Split is test
+# 2023-10-06 22:28:32.672 [1696631306926666/init_system/2 (pid 20851)] GPU available: False, used: False
+# 2023-10-06 22:28:32.672 [1696631306926666/init_system/2 (pid 20851)] TPU available: False, using: 0 TPU cores
+# 2023-10-06 22:28:32.672 [1696631306926666/init_system/2 (pid 20851)] IPU available: False, using: 0 IPUs
+# 2023-10-06 22:28:32.673 [1696631306926666/init_system/2 (pid 20851)] HPU available: False, using: 0 HPUs
+# 2023-10-06 22:28:54.058 [1696631306926666/init_system/2 (pid 20851)] Task finished successfully.
+# 2023-10-06 22:28:54.079 [1696631306926666/train_test/3 (pid 20954)] Task is starting.
+# 2023-10-06 22:29:01.309 [1696631306926666/train_test/3 (pid 20954)] Missing logger folder: /workspace/DCDL/course/week3/conflearn_project/lightning_logs
+# 2023-10-06 22:29:01.311 [1696631306926666/train_test/3 (pid 20954)] 
+# Epoch 1:  80%|████████  | 1600/2000 [00:25<00:06, 62.75it/s, loss=0.42, v_num=0, train_loss=0.354, train_acc=0.906, dev_loss=0.564, dev_acc=0.789]]
+# 2023-10-06 22:29:56.885 [1696631306926666/train_test/3 (pid 20954)] | Name  | Type       | Params|██████████| 400/400 [00:04<00:00, 105.16it/s]
+# 2023-10-06 22:29:56.885 [1696631306926666/train_test/3 (pid 20954)] -------------------------------------
+# 2023-10-06 22:29:56.886 [1696631306926666/train_test/3 (pid 20954)] 0 | model | Sequential | 49.3 K400 [00:00<?, ?it/s]
+# Epoch 1:  80%|████████  | 1601/2000 [00:25<00:06, 62.63it/s, loss=0.42, v_num=0, train_loss=0.354, train_acc=0.906, dev_loss=0.564, dev_acc=0.789]
+# Epoch 1:  80%|████████  | 1602/2000 [00:25<00:06, 62.66it/s, loss=0.42, v_num=0, train_loss=0.354, train_acc=0.906, dev_loss=0.564, dev_acc=0.789]
+# Epoch 1:  80%|████████  | 1603/2000 [00:25<00:06, 62.68it/s, loss=0.42, v_num=0, train_loss=0.354, train_acc=0.906, dev_loss=0.564, dev_acc=0.789]
+# Epoch 1:  80%|████████  | 1604/2000 [00:25<00:06, 62.71it/s, loss=0.42, v_num=0, train_loss=0.354, train_acc=0.906, dev_loss=0.564, dev_acc=0.789]
+# Epoch 1:  80%|████████  | 1605/2000 [00:25<00:06, 62.73it/s, loss=0.42, v_num=0, train_loss=0.354, train_acc=0.906, dev_loss=0.564, dev_acc=0.789]
+# Epoch 2:  80%|████████  | 1600/2000 [00:24<00:06, 66.44it/s, loss=0.346, v_num=0, train_loss=0.342, train_acc=0.875, dev_loss=0.432, dev_acc=0.830]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=0-step=1600.ckpt' as top 1
+# Epoch 3:  80%|████████  | 1600/2000 [00:23<00:05, 68.11it/s, loss=0.369, v_num=0, train_loss=0.379, train_acc=0.812, dev_loss=0.373, dev_acc=0.854]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=1-step=3200.ckpt' as top 1
+# Epoch 4:  80%|████████  | 1600/2000 [00:23<00:05, 68.13it/s, loss=0.316, v_num=0, train_loss=0.247, train_acc=0.938, dev_loss=0.351, dev_acc=0.851]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=2-step=4800.ckpt' as top 1
+# Epoch 5:  80%|████████  | 1600/2000 [00:26<00:06, 60.34it/s, loss=0.333, v_num=0, train_loss=0.200, train_acc=0.938, dev_loss=0.324, dev_acc=0.869]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=3-step=6400.ckpt' as top 1
+# Epoch 6:  80%|████████  | 1600/2000 [00:23<00:05, 69.28it/s, loss=0.283, v_num=0, train_loss=0.227, train_acc=0.969, dev_loss=0.320, dev_acc=0.866]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=4-step=8000.ckpt' as top 1
+# Epoch 7:  80%|████████  | 1600/2000 [00:23<00:05, 69.29it/s, loss=0.322, v_num=0, train_loss=0.265, train_acc=0.875, dev_loss=0.314, dev_acc=0.868]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=5-step=9600.ckpt' as top 1
+# Epoch 8:  80%|████████  | 1600/2000 [00:23<00:05, 68.17it/s, loss=0.324, v_num=0, train_loss=0.205, train_acc=0.906, dev_loss=0.305, dev_acc=0.878] del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=6-step=11200.ckpt' as top 1
+# Epoch 9:  80%|████████  | 1600/2000 [00:24<00:06, 65.87it/s, loss=0.29, v_num=0, train_loss=0.426, train_acc=0.750, dev_loss=0.290, dev_acc=0.882]] del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=7-step=12800.ckpt' as top 1
+# Epoch 9: 100%|██████████| 2000/2000 [00:28<00:00, 70.64it/s, loss=0.29, v_num=0, train_loss=0.426, train_acc=0.750, dev_loss=0.288, dev_acc=0.884]model to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=8-step=14400.ckpt' as top 1
+# 2023-10-06 22:34:15.727 [1696631306926666/train_test/3 (pid 20954)] Epoch 9, global step 16000: 'dev_loss' reached 0.28792 (best 0.28792), saving model to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=9-step=16000.ckpt' as top 1
+# 2023-10-06 22:34:15.731 [1696631306926666/train_test/3 (pid 20954)] Restoring states from the checkpoint path at /workspace/DCDL/course/week3/conflearn_project/log/epoch=9-step=16000.ckpt
+# 2023-10-06 22:34:15.783 [1696631306926666/train_test/3 (pid 20954)] Loaded model weights from checkpoint at /workspace/DCDL/course/week3/conflearn_project/log/epoch=9-step=16000.ckpt
+# Testing DataLoader 0: 100%|██████████| 500/500 [00:05<00:00, 95.58it/s] ing: 0it [00:00, ?it/s]
+# 2023-10-06 22:34:21.024 [1696631306926666/train_test/3 (pid 20954)] {'acc': 0.8872500061988831, 'loss': 0.29152730107307434} << FINAL ACC 0.88
+# 2023-10-06 22:35:24.446 [1696631306926666/train_test/3 (pid 20954)] Task finished successfully.
+# 2023-10-06 22:35:24.463 [1696631306926666/end/4 (pid 21873)] Task is starting.
+# 2023-10-06 22:35:26.855 [1696631306926666/end/4 (pid 21873)] done! great work!
+# 2023-10-06 22:35:27.276 [1696631306926666/end/4 (pid 21873)] Task finished successfully.
+# 2023-10-06 22:35:27.277 Done!s
--- course/week3/conflearn_project/flow_conflearn.py
+++ course/week3/conflearn_project/flow_conflearn.py
@@ -168,6 +168,25 @@ class TrainIdentifyReview(FlowSpec):
       # --
       # probs_: np.array[float] (shape: |test set|)
       # ===============================================
+
+      X_train = torch.from_numpy(X[train_index]).float()
+      X_test = torch.from_numpy(X[test_index]).float()
+      y_train = torch.from_numpy(y[train_index]).long()
+      y_test = torch.from_numpy(y[test_index]).long()
+
+      ds_train = TensorDataset(X_train, y_train)
+      ds_test = TensorDataset(X_test, y_test)
+
+      dl_train = DataLoader(ds_train, batch_size=self.config.train.optimizer.batch_size, shuffle=True)
+      dl_test = DataLoader(ds_test, batch_size=self.config.train.optimizer.batch_size, shuffle=False)
+
+      system = SentimentClassifierSystem(self.config)
+      trainer = Trainer(max_epochs=self.config.train.optimizer.max_epochs) # devices=1, accelerator="gpu" etc
+      trainer.fit(system, dl_train)
+
+      probs_ = trainer.predict(system, dataloaders=dl_test, ckpt_path = 'best')
+      probs_ = torch.cat(probs_).squeeze(1).numpy()
+
       assert probs_ is not None, "`probs_` is not defined."
       probs[test_index] = probs_
 
@@ -212,6 +231,10 @@ class TrainIdentifyReview(FlowSpec):
     # --
     # ranked_label_issues: List[int]
     # =============================
+    ranked_label_issues = find_label_issues(self.all_df.label,
+                                            prob,
+                                            return_indices_ranked_by='self_confidence'
+                                            )
     assert ranked_label_issues is not None, "`ranked_label_issues` not defined."
 
     # save this to class
@@ -308,6 +331,9 @@ class TrainIdentifyReview(FlowSpec):
     # dm.dev_dataset.data = dev slice of self.all_df
     # dm.test_dataset.data = test slice of self.all_df
     # # ====================================
+    dm.train_dataset.data = self.all_df.iloc[:train_size]
+    dm.dev_dataset.data = self.all_df.iloc[train_size:train_size+dev_size]
+    dm.test_dataset.data = self.all_df.iloc[train_size+dev_size:]
 
     # start from scratch
     system = SentimentClassifierSystem(self.config)
@@ -353,3 +379,132 @@ if __name__ == "__main__":
   You can specify a run id as well.
   """
   flow = TrainIdentifyReview()
+
+# first, download the big lfs files
+# cd data
+# sudo git lfs pull (you will have to provideyour github id and pw)
+
+# python flow_conflearn.py --no-pylint run
+# Metaflow 2.6.0 executing TrainIdentifyReview for user:gitpod
+# Validating your flow...
+#     The graph looks good!
+# 2023-10-06 22:37:59.757 Workflow starting (run-id 1696631879753337):
+# 2023-10-06 22:37:59.774 [1696631879753337/start/1 (pid 22638)] Task is starting.
+# 2023-10-06 22:38:02.571 [1696631879753337/start/1 (pid 22638)] Task finished successfully.
+# 2023-10-06 22:38:02.589 [1696631879753337/init_system/2 (pid 22694)] Task is starting.
+# 2023-10-06 22:38:05.203 [1696631879753337/init_system/2 (pid 22694)] GPU available: False, used: False
+# 2023-10-06 22:38:05.204 [1696631879753337/init_system/2 (pid 22694)] TPU available: False, using: 0 TPU cores
+# 2023-10-06 22:38:05.204 [1696631879753337/init_system/2 (pid 22694)] IPU available: False, using: 0 IPUs
+# 2023-10-06 22:38:05.204 [1696631879753337/init_system/2 (pid 22694)] HPU available: False, using: 0 HPUs
+# 2023-10-06 22:38:26.626 [1696631879753337/init_system/2 (pid 22694)] Task finished successfully.
+# 2023-10-06 22:38:26.646 [1696631879753337/train_test/3 (pid 22782)] Task is starting.
+# 2023-10-06 22:38:33.829 [1696631879753337/train_test/3 (pid 22782)] 
+# Epoch 1:  80%|████████  | 1600/2000 [00:25<00:06, 62.49it/s, loss=0.427, v_num=1, train_loss=0.596, train_acc=0.688, dev_loss=0.528, dev_acc=0.807]
+# 2023-10-06 22:39:27.488 [1696631879753337/train_test/3 (pid 22782)] | Name  | Type       | Params|██████████| 400/400 [00:04<00:00, 97.80it/s] 
+# 2023-10-06 22:39:27.489 [1696631879753337/train_test/3 (pid 22782)] -------------------------------------
+# 2023-10-06 22:39:27.489 [1696631879753337/train_test/3 (pid 22782)] 0 | model | Sequential | 49.3 K400 [00:00<?, ?it/s]
+# Epoch 1:  80%|████████  | 1601/2000 [00:25<00:06, 62.51it/s, loss=0.427, v_num=1, train_loss=0.596, train_acc=0.688, dev_loss=0.528, dev_acc=0.807]
+# Epoch 1:  80%|████████  | 1602/2000 [00:25<00:06, 62.41it/s, loss=0.427, v_num=1, train_loss=0.596, train_acc=0.688, dev_loss=0.528, dev_acc=0.807]
+# Epoch 1:  80%|████████  | 1603/2000 [00:25<00:06, 62.44it/s, loss=0.427, v_num=1, train_loss=0.596, train_acc=0.688, dev_loss=0.528, dev_acc=0.807]
+# Epoch 1:  80%|████████  | 1604/2000 [00:25<00:06, 62.46it/s, loss=0.427, v_num=1, train_loss=0.596, train_acc=0.688, dev_loss=0.528, dev_acc=0.807]
+# Epoch 1:  80%|████████  | 1605/2000 [00:25<00:06, 62.49it/s, loss=0.427, v_num=1, train_loss=0.596, train_acc=0.688, dev_loss=0.528, dev_acc=0.807]
+# Epoch 2:  80%|████████  | 1600/2000 [00:27<00:06, 58.68it/s, loss=0.367, v_num=1, train_loss=0.448, train_acc=0.781, dev_loss=0.405, dev_acc=0.840]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=0-step=1600.ckpt' as top 1
+# Epoch 3:  80%|████████  | 1600/2000 [00:25<00:06, 62.99it/s, loss=0.352, v_num=1, train_loss=0.532, train_acc=0.781, dev_loss=0.353, dev_acc=0.854]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=1-step=3200.ckpt' as top 1
+# Epoch 4:  80%|████████  | 1600/2000 [00:25<00:06, 62.29it/s, loss=0.317, v_num=1, train_loss=0.412, train_acc=0.781, dev_loss=0.366, dev_acc=0.833]del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=2-step=4800.ckpt' as top 1
+# Epoch 5:  80%|████████  | 1600/2000 [00:29<00:07, 53.82it/s, loss=0.312, v_num=1, train_loss=0.344, train_acc=0.844, dev_loss=0.360, dev_acc=0.834] 
+# Epoch 6:  80%|████████  | 1600/2000 [00:27<00:06, 58.41it/s, loss=0.288, v_num=1, train_loss=0.183, train_acc=0.969, dev_loss=0.308, dev_acc=0.871] 
+# Epoch 7:  80%|████████  | 1600/2000 [00:23<00:05, 68.97it/s, loss=0.299, v_num=1, train_loss=0.339, train_acc=0.844, dev_loss=0.292, dev_acc=0.880] el to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=5-step=9600.ckpt' as top 1
+# Epoch 8:  80%|████████  | 1600/2000 [00:24<00:06, 65.26it/s, loss=0.311, v_num=1, train_loss=0.576, train_acc=0.812, dev_loss=0.303, dev_acc=0.877] del to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=6-step=11200.ckpt' as top 1
+# Epoch 9:  80%|████████  | 1600/2000 [00:24<00:06, 66.12it/s, loss=0.303, v_num=1, train_loss=0.309, train_acc=0.812, dev_loss=0.298, dev_acc=0.876]
+# Epoch 9: 100%|██████████| 2000/2000 [00:28<00:00, 70.47it/s, loss=0.303, v_num=1, train_loss=0.309, train_acc=0.812, dev_loss=0.280, dev_acc=0.886]
+# 2023-10-06 22:43:35.984 [1696631879753337/train_test/3 (pid 22782)] Epoch 9, global step 16000: 'dev_loss' reached 0.28007 (best 0.28007), saving model to '/workspace/DCDL/course/week3/conflearn_project/log/epoch=9-step=16000-v1.ckpt' as top 1
+# 2023-10-06 22:43:35.988 [1696631879753337/train_test/3 (pid 22782)] Restoring states from the checkpoint path at /workspace/DCDL/course/week3/conflearn_project/log/epoch=9-step=16000-v1.ckpt
+# 2023-10-06 22:43:35.993 [1696631879753337/train_test/3 (pid 22782)] Loaded model weights from checkpoint at /workspace/DCDL/course/week3/conflearn_project/log/epoch=9-step=16000-v1.ckpt
+# Testing DataLoader 0: 100%|██████████| 500/500 [00:04<00:00, 101.68it/s]ing: 0it [00:00, ?it/s]
+# 2023-10-06 22:43:40.918 [1696631879753337/train_test/3 (pid 22782)] {'acc': 0.8899999856948853, 'loss': 0.2831442356109619}
+# 2023-10-06 22:44:44.170 [1696631879753337/train_test/3 (pid 22782)] Task finished successfully.
+# 2023-10-06 22:44:44.188 [1696631879753337/crossval/4 (pid 23580)] Task is starting.
+# 2023-10-06 22:44:51.419 [1696631879753337/crossval/4 (pid 23580)] GPU available: False, used: False
+# 2023-10-06 22:44:51.419 [1696631879753337/crossval/4 (pid 23580)] TPU available: False, using: 0 TPU cores
+# 2023-10-06 22:44:51.419 [1696631879753337/crossval/4 (pid 23580)] IPU available: False, using: 0 IPUs
+# 2023-10-06 22:44:51.419 [1696631879753337/crossval/4 (pid 23580)] HPU available: False, using: 0 HPUs
+# 2023-10-06 22:44:51.422 [1696631879753337/crossval/4 (pid 23580)] 
+# Epoch 9: 100%|██████████| 1667/1667 [00:12<00:00, 135.21it/s, loss=0.343, v_num=2, train_loss=0.533, train_acc=0.810]
+# 2023-10-06 22:47:27.123 [1696631879753337/crossval/4 (pid 23580)] | Name  | Type       | Params
+# Predicting DataLoader 0: 100%|██████████| 834/834 [00:34<00:00, -24.08it/s]   1667it [00:00, ?it/s]
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] -------------------------------------
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] 0 | model | Sequential | 49.3 K
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] -------------------------------------
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] 49.3 K    Trainable params
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] 0         Non-trainable params
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] 49.3 K    Total params
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] 0.197     Total estimated model params size (MB)
+# 2023-10-06 22:47:27.489 [1696631879753337/crossval/4 (pid 23580)] Restoring states from the checkpoint path at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_2/checkpoints/epoch=9-step=16670.ckpt
+# 2023-10-06 22:47:27.536 [1696631879753337/crossval/4 (pid 23580)] Loaded model weights from checkpoint at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_2/checkpoints/epoch=9-step=16670.ckpt
+# 2023-10-06 22:47:27.536 [1696631879753337/crossval/4 (pid 23580)] GPU available: False, used: False
+# 2023-10-06 22:47:27.538 [1696631879753337/crossval/4 (pid 23580)] TPU available: False, using: 0 TPU cores
+# 2023-10-06 22:47:27.538 [1696631879753337/crossval/4 (pid 23580)] IPU available: False, using: 0 IPUs
+# 2023-10-06 22:47:27.538 [1696631879753337/crossval/4 (pid 23580)] HPU available: False, using: 0 HPUs
+# 2023-10-06 22:47:27.538 [1696631879753337/crossval/4 (pid 23580)] 
+# Epoch 9: 100%|██████████| 1667/1667 [00:12<00:00, 132.36it/s, loss=0.287, v_num=3, train_loss=0.233, train_acc=0.952] 
+# 2023-10-06 22:49:32.487 [1696631879753337/crossval/4 (pid 23580)] | Name  | Type       | Params
+# Predicting DataLoader 0: 100%|██████████| 834/834 [00:02<00:00, -278.72it/s]  1667it [00:00, ?it/s]
+# 2023-10-06 22:49:32.609 [1696631879753337/crossval/4 (pid 23580)] -------------------------------------
+# 2023-10-06 22:49:32.609 [1696631879753337/crossval/4 (pid 23580)] 0 | model | Sequential | 49.3 K
+# 2023-10-06 22:49:32.609 [1696631879753337/crossval/4 (pid 23580)] -------------------------------------
+# 2023-10-06 22:49:32.609 [1696631879753337/crossval/4 (pid 23580)] 49.3 K    Trainable params
+# 2023-10-06 22:49:32.610 [1696631879753337/crossval/4 (pid 23580)] 0         Non-trainable params
+# 2023-10-06 22:49:32.610 [1696631879753337/crossval/4 (pid 23580)] 49.3 K    Total params
+# 2023-10-06 22:49:32.610 [1696631879753337/crossval/4 (pid 23580)] 0.197     Total estimated model params size (MB)
+# 2023-10-06 22:49:32.610 [1696631879753337/crossval/4 (pid 23580)] Restoring states from the checkpoint path at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_3/checkpoints/epoch=9-step=16670.ckpt
+# 2023-10-06 22:49:32.647 [1696631879753337/crossval/4 (pid 23580)] Loaded model weights from checkpoint at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_3/checkpoints/epoch=9-step=16670.ckpt
+# 2023-10-06 22:49:32.647 [1696631879753337/crossval/4 (pid 23580)] GPU available: False, used: False
+# 2023-10-06 22:49:32.650 [1696631879753337/crossval/4 (pid 23580)] TPU available: False, using: 0 TPU cores
+# 2023-10-06 22:49:32.650 [1696631879753337/crossval/4 (pid 23580)] IPU available: False, using: 0 IPUs
+# 2023-10-06 22:49:32.650 [1696631879753337/crossval/4 (pid 23580)] HPU available: False, using: 0 HPUs
+# 2023-10-06 22:49:32.650 [1696631879753337/crossval/4 (pid 23580)] 
+# Epoch 9: 100%|██████████| 1667/1667 [00:11<00:00, 143.48it/s, loss=0.309, v_num=4, train_loss=0.311, train_acc=0.864]
+# 2023-10-06 22:51:35.544 [1696631879753337/crossval/4 (pid 23580)] | Name  | Type       | Params
+# Predicting DataLoader 0: 100%|██████████| 834/834 [00:02<00:00, -286.66it/s]  1667it [00:00, ?it/s]
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] -------------------------------------
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] 0 | model | Sequential | 49.3 K
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] -------------------------------------
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] 49.3 K    Trainable params
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] 0         Non-trainable params
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] 49.3 K    Total params
+# 2023-10-06 22:51:35.626 [1696631879753337/crossval/4 (pid 23580)] 0.197     Total estimated model params size (MB)
+# 2023-10-06 22:51:35.627 [1696631879753337/crossval/4 (pid 23580)] Restoring states from the checkpoint path at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_4/checkpoints/epoch=9-step=16670.ckpt
+# 2023-10-06 22:51:58.088 [1696631879753337/crossval/4 (pid 23580)] Loaded model weights from checkpoint at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_4/checkpoints/epoch=9-step=16670.ckpt
+# 2023-10-06 22:51:58.090 [1696631879753337/crossval/4 (pid 23580)] Task finished successfully.
+# 2023-10-06 22:51:58.116 [1696631879753337/inspect/5 (pid 24108)] Task is starting.
+# 2023-10-06 22:52:01.252 [1696631879753337/inspect/5 (pid 24108)] 3951 label issues found.
+# 2023-10-06 22:52:03.526 [1696631879753337/inspect/5 (pid 24108)] Task finished successfully.
+# 2023-10-06 22:52:03.541 [1696631879753337/review/6 (pid 24194)] Task is starting.
+# 2023-10-06 22:52:06.984 [1696631879753337/review/6 (pid 24194)] Task finished successfully.
+# 2023-10-06 22:52:06.994 [1696631879753337/retrain_retest/7 (pid 24255)] Task is starting.
+# 2023-10-06 22:52:09.673 [1696631879753337/retrain_retest/7 (pid 24255)] GPU available: False, used: False
+# 2023-10-06 22:52:09.674 [1696631879753337/retrain_retest/7 (pid 24255)] TPU available: False, using: 0 TPU cores
+# 2023-10-06 22:52:09.674 [1696631879753337/retrain_retest/7 (pid 24255)] IPU available: False, using: 0 IPUs
+# 2023-10-06 22:52:09.677 [1696631879753337/retrain_retest/7 (pid 24255)] HPU available: False, using: 0 HPUs
+# 2023-10-06 22:52:09.677 [1696631879753337/retrain_retest/7 (pid 24255)] 
+# Epoch 5:  80%|████████  | 1600/2000 [00:24<00:06, 64.80it/s, loss=0.264, v_num=5, train_loss=0.314, train_acc=0.844, dev_loss=0.308, dev_acc=0.890]
+# 2023-10-06 22:54:59.585 1 tasks are running: e.g. ....st/7 (pid 24255)] Validation DataLoader 0: 100%|██████████| 400/400 [00:04<00:00, 74.34it/s]
+# 2023-10-06 22:54:59.585 0 tasks are waiting in the queue.
+# Epoch 9: 100%|██████████| 2000/2000 [00:29<00:00, 68.04it/s, loss=0.225, v_num=5, train_loss=0.185, train_acc=0.906, dev_loss=0.230, dev_acc=0.910] 
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] | Name  | Type       | Params|██████████| 400/400 [00:04<00:00, 83.36it/s]
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] -------------------------------------
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] 0 | model | Sequential | 49.3 K
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] -------------------------------------
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] 49.3 K    Trainable params
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] 0         Non-trainable params
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] 49.3 K    Total params
+# 2023-10-06 22:57:02.411 [1696631879753337/retrain_retest/7 (pid 24255)] 0.197     Total estimated model params size (MB)
+# 2023-10-06 22:57:02.412 [1696631879753337/retrain_retest/7 (pid 24255)] Restoring states from the checkpoint path at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_5/checkpoints/epoch=9-step=16000.ckpt
+# 2023-10-06 22:57:02.416 [1696631879753337/retrain_retest/7 (pid 24255)] Loaded model weights from checkpoint at /workspace/DCDL/course/week3/conflearn_project/lightning_logs/version_5/checkpoints/epoch=9-step=16000.ckpt
+# Testing DataLoader 0: 100%|██████████| 500/500 [00:05<00:00, 98.60it/s] Testing: 0it [00:00, ?it/s]
+# 2023-10-06 22:57:07.498 [1696631879753337/retrain_retest/7 (pid 24255)] {'acc': 0.9160624742507935, 'loss': 0.22987355291843414}.  <<<< IMPROVED ACC 0.92
+# 2023-10-06 22:57:07.924 [1696631879753337/retrain_retest/7 (pid 24255)] Task finished successfully.
+# 2023-10-06 22:57:07.940 [1696631879753337/end/8 (pid 24863)] Task is starting.
+# 2023-10-06 22:57:10.115 [1696631879753337/end/8 (pid 24863)] done! great work!
+# 2023-10-06 22:57:10.460 [1696631879753337/end/8 (pid 24863)] Task finished successfully.
+# 2023-10-06 22:57:10.461 Done!
